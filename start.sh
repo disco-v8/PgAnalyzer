df56b9ca1a559bf1d4081bfc505d3f6af13bdf42
T.Kabu <kabu@fvg-on.net>
2021-03-30 15:30:57 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ pkill -9 evs_pganalyzer
 rm -f /var/run/EvServer/.EvServer.sock
 rm -f /var/run/EvServer/.EvServer.pid
 rm -f /var/log/EvServer/EvServer.log
-rm -f /var/log/PgMulti/Startup.log
+rm -f /var/log/EvServer/Startup.log
 
 ./evs_pganalyzer ./evserver.ini
 ps awux | grep evs_pganalyzer
-cat /var/log/PgMulti/Startup.log
+cat /var/log/EvServer/Startup.log
 tail -F /var/log/EvServer/EvServer.log
